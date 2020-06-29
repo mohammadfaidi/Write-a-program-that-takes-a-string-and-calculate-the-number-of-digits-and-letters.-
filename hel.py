ff392bc3e2ca50c4e56c0b54d1fd76dea35c4642
mohammadfaidi <31220054+mohammadfaidi@users.noreply.github.com>
2020-06-29 19:15:47 +0300

    Update hel.py

--- hel.py
+++ hel.py
@@ -6,8 +6,24 @@ count2=0
 for i in string:
       if(i.isdigit()):
             count1=count1+1
-      count2=count2+1
+      else:
+        count2=count2+1
 print("The number of digits is:")
 print(count1)
 print("The number of characters is:")
 print(count2)
+
+
+
+'''
+list = [4,5,1,2]
+
+max = 0
+for i in list:
+    if max < i:
+        max= i
+
+print(max)
+'''
+
+
